5e09f9117061059d3c516937f847551478863fa4
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-12-22 00:59:49 -0800

    Updated CSS, Added 404 Component

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+// NotFound.js
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <section className="not-found-error">
+      <div className="inner-container x-small-width">
+        <h1>404 Not Found</h1>
+        <p className="green-accent uppercase">Sorry! The page you requested cannot be found.</p>
+        <p>If you typed the URL yourself, please make sure the spelling is correct. If you clicked on a link to get here, there may be a problem with the link. Try using your browser’s “Back” button or the search form below.</p>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
