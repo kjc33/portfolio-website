05e1d761da6a50064a68e1dd41a564027900f701
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-01-16 23:38:04 -0800

    Update Projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,6 +6,21 @@ export default function Work() {
   const featuredWorks1 = [
     {
       featuredWorkContainerClasses: "select-work-featured-bg project-1",
+      photographer: "Danny Howe",
+      photographerProfileLink: "https://unsplash.com/@dannyhowe?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+      featuredImgLink: "https://unsplash.com/photos/photo-of-music-band-performing-at-front-of-crowded-stadium-under-blue-sky-zZxs1_uupOQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+      featuredImgSrc: "Unsplash",
+      ProjectName: "Artify App Project",
+      projectDescription:
+        `Ritual Health and Fitness is owned and operated by Rod Stanley, an award-winning bodybuilder and fitness professional. The strategy, design, and site development came together naturally. I implemented a blog with custom taxonomy filters and a custom post for "Success Stories," utilizing Advanced Custom Fields (ACF) and Elementor's dynamic tagging.`,
+      projectLink: "https://spotify-artist-search-app.netlify.app/",
+      projectButtonLabel: "Visit Website",
+    },
+  ];
+
+  const featuredWorks2 = [
+    {
+      featuredWorkContainerClasses: "select-work-featured-bg project-2",
       photographer: "MSR",
       photographerProfileLink: "https://unsplash.com/@mvds?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
       featuredImgLink: "https://unsplash.com/photos/man-in-white-shirt-and-black-pants-carrying-black-and-yellow-skateboard-on-white-sand-during-fhQ816rFmN0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
@@ -17,9 +32,9 @@ export default function Work() {
     },
   ];
 
-  const featuredWorks2 = [
+  const featuredWorks3 = [
     {
-      featuredWorkContainerClasses: "select-work-featured-bg project-2",
+      featuredWorkContainerClasses: "select-work-featured-bg project-3",
       photographer: "Flo Pappert",
       photographerProfileLink: "https://unsplash.com/@flopt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
       featuredImgLink: "https://unsplash.com/photos/black-suv-park-beside-white-and-brown-house-during-daytime-kspRwqFxOms?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
@@ -32,9 +47,9 @@ export default function Work() {
     },
   ];
 
-  const featuredWorks3 = [
+  const featuredWorks4 = [
     {
-      featuredWorkContainerClasses: "select-work-featured-bg project-3",
+      featuredWorkContainerClasses: "select-work-featured-bg project-4",
       photographer: "AM",
       photographerProfileLink: "https://unsplash.com/@visualsbyroyalz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
       featuredImgLink: "https://unsplash.com/photos/man-in-orange-tank-top-and-black-shorts-doing-exercise-aclkvEMIfL8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
