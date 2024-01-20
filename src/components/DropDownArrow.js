b36e148ad2161811ecc256a419d3500b5deebfb1
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-01-19 18:36:41 -0800

    Updated Dropdown Arrow Button JSX

--- src/components/DropDownArrow.js
+++ src/components/DropDownArrow.js
@@ -1,14 +1,26 @@
-import React from "react";
+// import React from "react";
 
-import downArrow from "../images/arrow-white.svg";
+// import downArrow from "../images/arrow-white.svg";
+
+// export default function DropDownArrow({ href }) {
+//   return (
+//     <div className="drop-down-arrow">
+//       <a href={href}>
+//         <figure>
+//           <img className="arrow-icon" src={downArrow} alt="Drop Down Arrow" />
+//         </figure>
+//       </a>
+//     </div>
+//   );
+// }
+
+import React from "react";
 
 export default function DropDownArrow({ href }) {
   return (
     <div className="drop-down-arrow">
       <a href={href}>
-        <figure>
-          <img className="arrow-icon" src={downArrow} alt="Drop Down Arrow" />
-        </figure>
+        <div className="arrow-icon"></div>
       </a>
     </div>
   );
