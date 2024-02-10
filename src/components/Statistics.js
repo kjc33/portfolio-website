1d03d9fe0e57637b662d0ccd81793e97cab5cd8c
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-02-09 17:26:47 -0800

    Updated Years of Experience

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -11,7 +11,7 @@ export default function Statistics() {
     <div className="hexagon-divider no-bottom-padding">
       <div className="inner-container large-width flex-column flex-row-tablet large-gap">
         <Statistic hexagonContainerClasses="hexagon-statistic-bg hexagon-green" imgSrc={greenBorderHexagon} alt="Green Border Hexagon" num="150+" description="Websites Built" />
-        <Statistic hexagonContainerClasses="hexagon-statistic-bg hexagon-white" imgSrc={whiteHexagon} alt="White Hexagon" num="10" description="Years of Experience" />
+        <Statistic hexagonContainerClasses="hexagon-statistic-bg hexagon-white" imgSrc={whiteHexagon} alt="White Hexagon" num="11" description="Years of Experience" />
         <Statistic hexagonContainerClasses="hexagon-statistic-bg hexagon-black" imgSrc={blackHexagon} alt="Black Hexagon" num="6" description="Developer Certifications" />
       </div>
     </div>
