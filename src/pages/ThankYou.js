f21a50f88d25b1228be7761ff9e166711463e96a
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-12 17:50:24 -0700

    Updated Thank You and Not Found Styling

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -4,9 +4,11 @@ import PrimaryButton from "../components/PrimaryButton";
 function ThankYou() {
   return (
     <section className="thank-you">
-      <div className="inner-container flex-column small-gap large-text underline-link x-small-width">
-        <h1>Thank You</h1>
-        <p>Thank you for contacting me. Please allow 24-48 hours for a response. I look forward to connecting with you soon.</p>
+      <div className="inner-container flex-column small-gap x-small-width">
+        <h1 className="primary-h1 white">Thank You</h1>
+        <div className="large-text underline-link">
+          <p>Thank you for contacting me. Please allow 24-48 hours for a response. I look forward to connecting with you soon.</p>
+        </div>
         <PrimaryButton url="/" label="Homepage" />
       </div>
     </section>
