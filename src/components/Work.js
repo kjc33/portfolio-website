0d736974110e238323a95738593981bec3c2173d
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-12 23:32:26 -0700

    Updated Projects and Added Quizer

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,7 +11,7 @@ export default function Work() {
       featuredImgLink: "https://unsplash.com/photos/man-in-white-t-shirt-C7DWZcxFCNY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
       featuredImgSrc: "Unsplash",
       ProjectName: "Artify Search App",
-      projectDescription: `Artify is an artist search app that helps users learn more about the musicians they love. Built in React, Artify uses the Spotify API to display the artist's name, number of followers, genres, and top songs. Unfortunately, the Spotify API doesn't include biographies, so Artify also uses the Wikipedia API to expand upon artist profiles.`,
+      projectDescription: `Artify is an artist search app that helps users learn more about the musicians they love. Built in React, Artify uses the Spotify API to display the artist's name, number of followers, genres, and top songs. Unfortunately, the Spotify API doesn't include biographies, so Artify also pulls from the Wikipedia API to provide background information about the artists.`,
       projectLink: "https://spotify-artist-search-app.netlify.app/",
       projectButtonLabel: "Visit Website",
     },
@@ -20,13 +20,13 @@ export default function Work() {
   const featuredWorks2 = [
     {
       featuredWorkContainerClasses: "select-work-featured-bg project-2",
-      photographer: "MSR",
-      photographerProfileLink: "https://unsplash.com/@mvds?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-      featuredImgLink: "https://unsplash.com/photos/man-in-white-shirt-and-black-pants-carrying-black-and-yellow-skateboard-on-white-sand-during-fhQ816rFmN0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+      photographer: "TC",
+      photographerProfileLink: "https://unsplash.com/@thoughtcatalog?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+      featuredImgLink: "https://unsplash.com/photos/woman-sitting-on-floor-and-leaning-on-couch-using-laptop-Nv-vx3kUR2A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
       featuredImgSrc: "Unsplash",
-      ProjectName: "The Chill Foundation",
-      projectDescription: `As a youth sports nonprofit in Vermont, I co-designed and led the development of The Chill Foundation's new website in 2022. I worked from an approved design and designed other pages during development. Integrating the homepage "Programs" section with hover-over images and click-to-landing functionality pushed the limits of Elementor.`,
-      projectLink: "https://www.chill.org",
+      ProjectName: "Quizer Challenge",
+      projectDescription: `Quizer is a full-stack React application for trivia enthusiasts. The exam features a countdown timer that automatically tallies an incorrect response if no answer is selected before the time expires. Using protected routes, Quizer also has an admin Dashboard for editing the quiz questions and answers as well as a dynamic leaderboard for the top scores.`,
+      projectLink: "https://quizer-application.netlify.app/",
       projectButtonLabel: "Visit Website",
     },
   ];
@@ -34,20 +34,6 @@ export default function Work() {
   const featuredWorks3 = [
     {
       featuredWorkContainerClasses: "select-work-featured-bg project-3",
-      photographer: "Flo Pappert",
-      photographerProfileLink: "https://unsplash.com/@flopt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-      featuredImgLink: "https://unsplash.com/photos/black-suv-park-beside-white-and-brown-house-during-daytime-kspRwqFxOms?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-      featuredImgSrc: "Unsplash",
-      ProjectName: "Mountainwood Homes",
-      projectDescription: `Mountainwood Homes, a premier home builder and renovation company, tasked me to lead the entire redesign project, including AM, strategy, design, and development. I explored overlapping elements, vertical text, and image "cutouts" to mimic crown moulding. This site has aged nicely over the years, as my core design is mostly still intact.`,
-      projectLink: "https://www.mountainwoodhomes.com",
-      projectButtonLabel: "Visit Website",
-    },
-  ];
-
-  const featuredWorks4 = [
-    {
-      featuredWorkContainerClasses: "select-work-featured-bg project-4",
       photographer: "AM",
       photographerProfileLink: "https://unsplash.com/@visualsbyroyalz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
       featuredImgLink: "https://unsplash.com/photos/man-in-orange-tank-top-and-black-shorts-doing-exercise-aclkvEMIfL8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
@@ -72,9 +58,6 @@ export default function Work() {
           ))}
           {featuredWorks3.map((featuredWork3, index) => (
             <WorkListingItem key={index} featuredWorkDetails={featuredWork3} />
-          ))}
-          {featuredWorks4.map((featuredWork4, index) => (
-            <WorkListingItem key={index} featuredWorkDetails={featuredWork4} />
           ))}
         </div>
       </div>
