1bbbf32746ac0b8c6e167fa6dc1785541323cfea
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-12 17:37:08 -0700

    Update Not Found and Thank You Section Classes

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -4,9 +4,9 @@ import PrimaryButton from "../components/PrimaryButton";
 function ThankYou() {
   return (
     <section className="thank-you">
-      <div className="inner-container flex-column small-gap x-small-width">
+      <div className="inner-container flex-column small-gap large-text underline-link x-small-width">
         <h1>Thank You</h1>
-        <p className="large-text underline-link">Thank you for contacting me. Please allow 24-48 hours for a response. I look forward to connecting with you soon.</p>
+        <p>Thank you for contacting me. Please allow 24-48 hours for a response. I look forward to connecting with you soon.</p>
         <PrimaryButton url="/" label="Homepage" />
       </div>
     </section>
