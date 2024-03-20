9e4577732626a80845ace94cc941da0cb78d6482
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-19 21:00:46 -0500

    removed unused slices of state

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 export default function ContactForm() {
-  const [submitted, setSubmitted] = useState(false);
-  const [validationErrors, setValidationErrors] = useState({});
   const navigate = useNavigate(); // Initialize useNavigate
-
   const API_URL = process.env.REACT_APP_API_URL;
 
   return (
@@ -38,7 +35,6 @@ export default function ContactForm() {
           }
           return errors;
         }}
-
         onSubmit={(values, { setSubmitting, resetForm, setErrors }) => {
           fetch(`${API_URL}/api/contact-form`, {
             method: "POST",
@@ -64,44 +60,50 @@ export default function ContactForm() {
             });
         }}
       >
-        {({ isSubmitting }) => (
+        {({ isSubmitting, errors, touched }) => (
           <Form className="contact-form display-grid" id="contact-form">
-            <div className={"field-wrapper first-name display-grid" + (submitted && validationErrors.first_name ? " error" : "")}>
+            <div className={"field-wrapper first-name display-grid" + (touched.first_name && errors.first_name ? " error" : "")}>
               <label htmlFor="first_name">First Name</label>
               <Field type="text" name="first_name" id="first_name" placeholder="First Name" autoComplete="on" />
               <ErrorMessage name="first_name" component="div" className="error-message" />
             </div>
-            <div className={"field-wrapper last-name display-grid" + (submitted && validationErrors.last_name ? " error" : "")}>
+            <div className={"field-wrapper last-name display-grid" + (touched.last_name && errors.last_name ? " error" : "")}>
               <label htmlFor="last_name">Last Name</label>
               <Field type="text" name="last_name" id="last_name" placeholder="Last Name" autoComplete="on" />
               <ErrorMessage name="last_name" component="div" className="error-message" />
             </div>
-            <div className={"field-wrapper email display-grid" + (submitted && validationErrors.email ? " error" : "")}>
+            <div className={"field-wrapper email display-grid" + (touched.email && errors.email ? " error" : "")}>
               <label htmlFor="email">Email</label>
               <Field type="email" name="email" id="email" placeholder="Email" autoComplete="on" />
               <ErrorMessage name="email" component="div" className="error-message" />
             </div>
-            <div className={"field-wrapper phone display-grid" + (submitted && validationErrors.phone ? " error" : "")}>
+            <div className={"field-wrapper phone display-grid" + (touched.phone && errors.phone ? " error" : "")}>
               <label htmlFor="phone">Phone</label>
               <Field type="tel" name="phone" id="phone" placeholder="Phone" autoComplete="on" />
               <ErrorMessage name="phone" component="div" className="error-message" />
             </div>
-            <div className="field-wrapper website display-grid">
+            <div className={"field-wrapper website display-grid" + (touched.website && errors.website ? " error" : "")}>
               <label htmlFor="website">Website</label>
-              <Field type="url" name="website" id="website" placeholder="Website" />
+              <Field type="text" name="website" id="website" placeholder="Website" autoComplete="on" />
               <ErrorMessage name="website" component="div" className="error-message" />
             </div>
-            <div className="field-wrapper message display-grid">
+            <div className={"field-wrapper message display-grid" + (touched.message && errors.message ? " error" : "")}>
               <label htmlFor="message">Message</label>
-              <Field as="textarea" name="message" id="message" cols="30" rows="10" placeholder="Tell me about your website goals, budget, and any other important project details." />
+              <Field as="textarea" name="message" id="message" placeholder="Message" autoComplete="on" />
               <ErrorMessage name="message" component="div" className="error-message" />
             </div>
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
+            <div className="button-wrapper">
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+                {isSubmitting && (
+                  <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                )}
+              </button>
+            </div>
           </Form>
         )}
       </Formik>
     </div>
   );
 }
+
