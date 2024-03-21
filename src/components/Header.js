70fe9d0d1c5baaa7d13ee9cd4980425f9bc9c639
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-20 20:36:13 -0700

    Update Header Scroll Classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import SiteLogo from "./SiteLogo";
 import PrimaryNav from "./PrimaryNav";
@@ -8,8 +8,27 @@ import PrimaryButton from "./PrimaryButton";
 import headshot from "../images/kyle-chin-headshot.jpg";
 
 export default function Header() {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      if (currentScrollPos > 0) {
+        setScrollDirection("scrolling-down");
+      } else {
+        setScrollDirection("scrolling-up");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="primary-header">
+    <header className={`primary-header ${scrollDirection}`}>
       <div className="header-inner">
         <SiteLogo headshot={headshot} siteName="Kyle Chin" alt="Kyle Chin Headshot" width="80" height="80" />
         <PrimaryNav />
