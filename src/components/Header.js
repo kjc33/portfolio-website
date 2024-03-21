5fb1d5d576f8acbc31ed0642abbb22a7bc31c451
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-20 21:01:00 -0700

    Update Scroll Logic Classes and Depth

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,22 @@ import headshot from "../images/kyle-chin-headshot.jpg";
 
 export default function Header() {
   const [scrollDirection, setScrollDirection] = useState(null);
+  const [scrolledEnough, setScrolledEnough] = useState(false);
 
   useEffect(() => {
     let lastScrollPosition = window.pageYOffset;
 
     const handleScroll = () => {
       const currentScrollPos = window.pageYOffset;
-      if (currentScrollPos > lastScrollPosition) {
-        setScrollDirection("scroll-down");
-      } else {
-        setScrollDirection("scroll-up");
+      if (!scrolledEnough && currentScrollPos > 111) {
+        setScrolledEnough(true);
+      }
+      if (scrolledEnough) {
+        if (currentScrollPos > lastScrollPosition) {
+          setScrollDirection("scroll-down");
+        } else {
+          setScrollDirection("scroll-up");
+        }
       }
       lastScrollPosition = currentScrollPos;
     };
@@ -28,10 +34,10 @@ export default function Header() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [scrolledEnough]);
 
   return (
-    <header className={`primary-header ${scrollDirection}`}>
+    <header className={`primary-header ${scrolledEnough ? scrollDirection : ""}`}>
       <div className="header-inner">
         <SiteLogo headshot={headshot} siteName="Kyle Chin" alt="Kyle Chin Headshot" width="80" height="80" />
         <PrimaryNav />
