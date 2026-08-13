b326a92c981737683cf7e5008b1f074d35ed802b
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2026-08-13 02:04:45 -0700

    Update footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
             />
           </div>
         </div>
-        <div className="footer-middle full-width flex column-reverse justify-space-between align-top xx-large-gap">
+        <div className="footer-middle full-width flex-column column-reverse justify-space-between align-top small-gap">
           <div className="accessibility-disclaimer underline-link">
             <p>
               I am dedicated to providing a website that is accessible to all users and aim to align with WCAG 2.1 Level AA standards. If you encounter any accessibility barriers or require content in an alternative format, please <HashLink smooth to="#contact">contact</HashLink> me, and I will work with you to provide a reasonable accommodation.
@@ -54,7 +54,7 @@ export default function Footer() {
             />
           </div>
         </div>
-        <div className="footer-bottom full-width flex column-reverse justify-space-between align-top large-gap">
+        <div className="footer-bottom full-width column-reverse justify-space-between align-top small-gap">
           <div className="footer-signature">
             <p>
               &copy; {copyrightDate} Kyle John Chin. All rights reserved. <span className="pipe">|</span> Website by me, duh.
